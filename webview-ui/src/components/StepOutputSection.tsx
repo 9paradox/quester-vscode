e35f3db47e53c052e363500035fdc8580cea912d
Paradox <paradox.akshay@gmail.com>
2024-04-06 23:03:15 +0530

    added json selected path

--- webview-ui/src/components/StepOutputSection.tsx
+++ webview-ui/src/components/StepOutputSection.tsx
@@ -1,11 +1,46 @@
-import { Card, ScrollArea, Text, Stack, Center, Group, Textarea } from "@mantine/core";
+import { Card, ScrollArea, Text, Stack, Center, Group, Textarea, TextInput } from "@mantine/core";
 import { useSteps } from "../Store";
 import { IconClick } from "@tabler/icons-react";
 import ReactJson from "@microlink/react-json-view";
+import { useEffect, useState } from "react";
+
+function getPropertyPath(namespaces: Array<string | null>, finalNamespace?: string | null): string {
+  if (namespaces.length === 0 && finalNamespace === null) {
+    return "";
+  }
+
+  if (finalNamespace) {
+    namespaces.push(finalNamespace);
+  }
+
+  var path = "";
+
+  for (let i = 0; i < namespaces.length; i++) {
+    if (isNaN(Number(namespaces[i]))) {
+      path += namespaces[i];
+    } else {
+      path += `[${namespaces[i]}]`;
+    }
+
+    if (i + 1 < namespaces.length && namespaces[i + 1] !== null) {
+      if (isNaN(Number(namespaces[i + 1]))) {
+        path += ".";
+      }
+    }
+  }
+
+  return path;
+}
 
 function StepOutputSection() {
   const { stepResults, selectedStep } = useSteps();
   const selectedStepResult = selectedStep ? stepResults[selectedStep.index] : undefined;
+  const [path, setPath] = useState<string>("");
+
+  useEffect(() => {
+    setPath("");
+  }, [selectedStep]);
+
   return (
     <Card shadow="none" withBorder radius="md" h="calc(100vh - 40px)" p="md">
       <Card.Section p="lg">
@@ -38,7 +73,14 @@ function StepOutputSection() {
               name="timeTaken"
               style={{ marginBottom: "8px", padding: "6px", borderRadius: "4px" }}
             />
-
+            {path && (
+              <TextInput
+                readOnly={true}
+                label="Selected Path"
+                value={path}
+                style={{ marginBottom: "8px", padding: "6px", borderRadius: "4px" }}
+              />
+            )}
             {selectedStepResult?.result?.step?.outputData instanceof Object && (
               <ReactJson
                 src={selectedStepResult?.result?.step?.outputData}
@@ -48,6 +90,15 @@ function StepOutputSection() {
                 displayObjectSize={false}
                 enableClipboard={false}
                 name="outputData"
+                onSelect={(s) => {
+                  console.log(s);
+                  if (s.namespace) {
+                    const path = getPropertyPath(s.namespace, s.name);
+                    if (path) {
+                      setPath(path);
+                    }
+                  }
+                }}
                 style={{ marginBottom: "8px", padding: "6px", borderRadius: "4px" }}
               />
             )}
