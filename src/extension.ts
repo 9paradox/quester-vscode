fb650eaafe63c557f49ba0d3fef42e01c5121fb3
Paradox <paradox.akshay@gmail.com>
2024-04-13 11:54:56 +0530

    added create new apitester file command

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,44 @@
-import { commands, ExtensionContext, window } from "vscode";
+import { commands, ExtensionContext, Uri, window } from "vscode";
 import { ApiTesterEditor } from "./panels/ApiTesterEditor";
 import { ApiTesterRunner } from "./panels/ApiTesterRunner";
+import * as fs from "fs";
+import * as path from "path";
 
 export function activate(context: ExtensionContext) {
   context.subscriptions.push(ApiTesterEditor.register(context));
 
-  const showHelloWorldCommand = commands.registerCommand(
+  const questerRunnerCommand = commands.registerCommand(
     "quester.apitesterRunner.open",
     (...args) => {
       ApiTesterRunner.render(context.extensionUri, args[0]);
     }
   );
 
-  context.subscriptions.push(showHelloWorldCommand);
+  context.subscriptions.push(questerRunnerCommand);
+
+  let createFileCommand = commands.registerCommand("quester.apitesterEditor.new", (...args) => {
+    createApitesterFile(args[0]);
+  });
+
+  context.subscriptions.push(createFileCommand);
+}
+
+async function createApitesterFile(folderUri: Uri) {
+  const folderPath = folderUri.fsPath;
+  const inputFileName = await window.showInputBox({
+    prompt: "Enter the name for the new apitester file (without any extension)",
+    validateInput: (value) => {
+      if (!value.trim()) {
+        return "File name cannot be empty";
+      }
+      return null;
+    },
+  });
+
+  if (inputFileName) {
+    const testCaseFileName = inputFileName.trim().replace(/\s+/g, "-").toLowerCase();
+    const newJsonFilePath = path.join(folderPath, testCaseFileName + ".apitester");
+    fs.writeFileSync(newJsonFilePath, `{ "title": "${inputFileName}", "steps": [] }`);
+    window.showInformationMessage("New apitester file created successfully!");
+  }
 }
